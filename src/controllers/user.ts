721206938280b00320f1a8f455261fc3ab64640d
Oz <ozcancey20@gmail.com>
2022-11-20 17:55:28 -0500

    modified create user method

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import User from "../models/user";
+import * as auth_util from '../utils/auth_util';
 
 export const getUsers = async (req: Request, res: Response) => {
     let users;
@@ -23,26 +24,44 @@ export const getUserById = async (req: Request, res: Response) => {
     return res.status(200).json({ user: user });
 };
 
+
 export const createUser = async (req: Request, res: Response) => {
     let user;
     try {
-        user = await User.create(req.body)
-    } catch (error) {
-        return res.status(400).json({ message: 'An error occurred while creating user' });
-    }
+        const { name, email, password, role } = req.body;
+        const hash = await auth_util.hashPassword(password);
 
-    return res.status(201).json({ user: user });
+        user = await User.create({
+            name: name,
+            email: email,
+            password: hash,
+            resetPasswordToken: '',
+            resetPasswordExpire: new Date(),
+            role: role
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            message: 'An error occurred while registering user',
+            error: error
+        });
+    }
+    return res.status(201).json({
+        message: 'User cerated successfully',
+        data: user
+    });
 };
 
 export const updateUser = async (req: Request, res: Response) => {
     const { name, email, password, role } = req.body;
+    const hash = await auth_util.hashPassword(password);
     let user;
     try {
         user = await User.update({
-            name,
-            email,
-            password,
-            role
+            name: name,
+            email: email,
+            password: hash,
+            role: role
         }, {
             where: { id: req.params.id }
         });
@@ -51,7 +70,7 @@ export const updateUser = async (req: Request, res: Response) => {
     }
 
     return res.status(200).json({
-        message: 'User updated successfully!',
+        message: 'User updated successfully!'
     });
 };
 
