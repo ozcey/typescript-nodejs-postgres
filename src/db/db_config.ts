2defa7ac2c300504f6272c3e4b593c5fd8f26baa
Oz <ozcancey20@gmail.com>
2022-12-16 22:45:04 -0500

    disabled sql logging

--- src/db/db_config.ts
+++ src/db/db_config.ts
@@ -3,7 +3,8 @@ import 'dotenv/config';
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS, {
     dialect: 'postgres',
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST,
+    logging: false
 });
 
 export default sequelize;
